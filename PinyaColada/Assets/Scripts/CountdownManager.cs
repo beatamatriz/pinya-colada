7ce8872a1feb734c4db11cc33f984e8c295436d3
Bea <31886814+beatamatriz@users.noreply.github.com>
2023-07-29 11:15:45 +0200

    countdown

---
+++ PinyaColada/Assets/Scripts/CountdownManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountdownManager : MonoBehaviour{
+    [Range(1, 1800)]
+    public int time_remaining;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
