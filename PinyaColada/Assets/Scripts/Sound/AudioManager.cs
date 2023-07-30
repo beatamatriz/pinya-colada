4de7e67f857c848928620ca85629e7c2b54dab3a
Ferboide <83360587+Ferboide@users.noreply.github.com>
2023-07-30 10:36:36 +0200

    AudioManager y clase Sound
    Un sistema para implementar sonidos

---
+++ PinyaColada/Assets/Scripts/Sound/AudioManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AudioManager : MonoBehaviour {
+    public List<Sound> sounds;
+
+    private void Awake() {
+        foreach (Sound s in sounds) {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    public void Play(string name) {
+        foreach (Sound s in sounds) {
+            if (s.name == name) {
+                s.source.Play();
+            }
+        }
+    }
+}
