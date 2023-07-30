f6e734410cb8e2cba6b21c1dfeba238d33540074
Jorge <perortico@gmail.com>
2023-07-30 15:55:29 +0200

    sorted backtext black mancha and put finals jpeg

--- PinyaColada/Assets/Scripts/InteractableObject.cs
+++ PinyaColada/Assets/Scripts/InteractableObject.cs
@@ -1,75 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class InteractableObject : MonoBehaviour {
+public class InteractableObject : MonoBehaviour
+{
     [TextArea] public string objText;
     int objTextIndex;
     public float deactivationSafe;
-
     public float txtSpeed;
     private float counter;
-
     public GameObject bGPanel;
     public float txtLifetime;
     public bool endWritingFirst = false;
-
     bool isActive = false;
-
-    public void CallInteraction() {
-        if (!isActive) {
+    public void CallInteraction()
+    {
+        if (!isActive)
+        {
             ManagerRefs.instance.gM.objTextbox.text = "";
             counter = txtSpeed;
             objTextIndex = 0;
-
             isActive = true;
         }
     }
-
-    private void Update() {
-        if (isActive && ManagerRefs.instance.gM.objTextbox.text != objText) {
+    private void Update()
+    {
+        if (isActive && ManagerRefs.instance.gM.objTextbox.text != objText)
+        {
             //Completar texto poco a poco.
             counter -= Time.deltaTime;
-
-            if (counter <= 0) {
-                ManagerRefs.instance.gM.objTextbox.text += objText[objTextIndex];
-                objTextIndex++;
-
+            if (counter <= 0)
+            {
+                if (objTextIndex < objText.Length)
+                {
+                    ManagerRefs.instance.gM.objTextbox.text += objText[objTextIndex];
+                    objTextIndex++;
+                }
                 counter = txtSpeed;
             }
-
-            if (Input.GetMouseButtonDown(0)) {
+            if (Input.GetMouseButtonDown(0))
+            {
                 ManagerRefs.instance.gM.objTextbox.text = objText;
             }
-
-        } else if (isActive && ManagerRefs.instance.gM.objTextbox.text == objText) {
-            if (!endWritingFirst) {
+        }
+        else if (isActive && ManagerRefs.instance.gM.objTextbox.text == objText)
+        {
+            if (!endWritingFirst)
+            {
                 endWritingFirst = true;
             }
-
-            if (Input.GetMouseButtonDown(0)) {
+            if (Input.GetMouseButtonDown(0))
+            {
                 StartCoroutine(DeactivateWait());
             }
         }
-
-        if (ManagerRefs.instance.gM.objTextbox.text == "") {
+        if (ManagerRefs.instance.gM.objTextbox.text == "")
+        {
             bGPanel.SetActive(false);
-        } else {
+        }
+        else
+        {
             bGPanel.SetActive(true);
         }
     }
-
-    IEnumerator DeactivateWait() {
+    IEnumerator DeactivateWait()
+    {
         yield return new WaitForSeconds(deactivationSafe);
-
         ManagerRefs.instance.gM.objTextbox.text = "";
-
         isActive = false;
     }
-
-    IEnumerator TextLife() {
+    IEnumerator TextLife()
+    {
         yield return new WaitForSeconds(txtLifetime);
-
         ManagerRefs.instance.gM.objTextbox.text = "";
     }
 }
